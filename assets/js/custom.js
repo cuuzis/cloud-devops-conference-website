b788d535ab1da8e3a5ff9fcec4a2e500fa50c697
Gustavs Venters <gustavs.venters@gmail.com>
2017-11-26 16:01:05 +0100

    DST mistake

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,7 +42,7 @@
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 
-	$('#mu-event-counter').countdown('2017/12/15 16:00:00').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2017/12/15 17:00:00').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
